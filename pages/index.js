267954b41e7fd6f746f9fcb3a93ee324dc0779e0
Jonathan Sexton <joncsexton@gmail.com>
2020-08-29 21:59:32 -0500

    finally got the image optimizer to work

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Image from '../components/Image/image';
 
 export default function Home() {
   return (
     <article>
-      <h1 className='text-5xl'>Welcome to Luna Repair System!</h1>
+      <h1 className="text-5xl">Welcome to Luna Repair System!</h1>
+      <div>
+        <Image path={`landingPageBG.jpg`} />
+      </div>
     </article>
   );
 }
