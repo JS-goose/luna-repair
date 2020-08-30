ae699c037dc81f1849b58cf54e62ecef07993361
Jonathan Sexton <joncsexton@gmail.com>
2020-08-30 11:17:35 -0500

    added color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: `#355D87`,
+        medDarkBlue: `#3171B5`,
+        lightBlue: `#2A98EE`,
+        eggShell: `#F7F7F7`,
+        lime: `#12C73D`,
+        liteRed: `#DC3545`,
+        charcoal: `#232323`
+      },
+    },
   },
   variants: {},
   plugins: [],
   future: {
     removeDeprecatedGapUtilities: true,
-  }
-}
+  },
+};
