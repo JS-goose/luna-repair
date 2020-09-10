713b2c79a6338f15af2ee9b4902487a4e7ca55dc
Jonathan Sexton <joncsexton@gmail.com>
2020-09-10 07:25:40 -0500

    added color and basic positioning

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { providers, signIn } from 'next-auth/client';
+import Image from '../../components/Image/image';
 
 export default function SignIn({ providers }) {
   return (
-    <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button
-            onClick={() =>
-              signIn(provider.id, { callbackUrl: 'http://localhost:3000/create-invoice' })
-            }
-          >
-            Sign in with {provider.name} and bacon!
-          </button>
-        </div>
-      ))}
-    </>
+    <section className="signin-container text-white flex flex-col items-center justify-center">
+      <article className="bg-medDarkBlue p-6 text-xl">
+        {Object.values(providers).map((provider) => (
+          <div key={provider.name} className="flex flex-col justify-center p-4 border border-black">
+            <button
+              onClick={() =>
+                signIn(provider.id, { callbackUrl: 'http://localhost:3000/create-invoice' })
+              }
+            >
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
+      </article>
+    </section>
   );
 }
 
