5e2b3c57b545066c969f34a3efcddcac5052abff
Stu Finn <stufinn@gmail.com>
2020-09-10 01:36:45 -0500

    Create basic invoice page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        dashboard: '250px 1fr',
+      },
       colors: {
         darkBlue: `#355D87`,
         medDarkBlue: `#3171B5`,
@@ -10,13 +13,13 @@ module.exports = {
         lime: `#12C73D`,
         liteRed: `#DC3545`,
         charcoal: `#232323`,
-        wht20: `rgba(255, 255, 255, 0.2)`
+        wht20: `rgba(255, 255, 255, 0.2)`,
       },
     },
   },
   variants: {
-    backgroundColor: [`responsive`,`hover`,`focus`,`active`],
-    textColor: [`responsive`,`hover`,`focus`,`active`]
+    backgroundColor: [`responsive`, `hover`, `focus`, `active`],
+    textColor: [`responsive`, `hover`, `focus`, `active`],
   },
   plugins: [],
   future: {
