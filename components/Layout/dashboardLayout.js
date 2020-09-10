b7626a9fb3bebb86f16bfc25930bdcb13cd4aa18
Stu Finn <stufinn@gmail.com>
2020-09-10 01:38:35 -0500

    Add logo placeholder text

--- components/Layout/dashboardLayout.js
+++ components/Layout/dashboardLayout.js
@@ -3,13 +3,14 @@
 import React from 'react';
 
 const SidebarItem = ({ children }) => (
-  <div className="my-5 ml-5">{children}</div>
+  <div className="my-5 ml-5 text-xl">{children}</div>
 );
 
 const DashboardLayout = ({ children }) => {
   return (
     <div className="grid grid-flow-col grid-cols-dashboard h-screen">
       <div className="flex flex-col justify-left bg-blue-800 max-h-full text-white p-3">
+        <div className="text-3xl mb-5 ml-3">LOGO HERE</div>
         <SidebarItem>Dashboard</SidebarItem>
         <SidebarItem>Customers</SidebarItem>
         <SidebarItem>Invoices</SidebarItem>
