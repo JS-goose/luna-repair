8f3a455d9df658c1af16932ad816edf1a316fe7e
Jonathan Sexton <joncsexton@gmail.com>
2020-09-13 15:39:56 -0500

    added attribution for background image

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import { signIn, signOut, useSession, options } from 'next-auth/client';
-import styles from '../styles/Home.module.css';
-import Image from '../components/Image/image';
-import Layout from '../components/Layout/layout';
+import Head from "next/head";
+import Link from "next/link";
+import { signIn, signOut, useSession, options } from "next-auth/client";
+import styles from "../styles/Home.module.css";
+import Image from "../components/Image/image";
+import Layout from "../components/Layout/layout";
 
 export default function Home() {
   const [session, loading] = useSession();
@@ -11,10 +11,9 @@ export default function Home() {
   return (
     <Layout>
       <section className="landing-page-container flex flex-col items-center justify-center sm:space-y-10 md:space-y-40">
+        {/* Background image courtesy of Living Smarter vis Unsplash https://unsplash.com/@livingsmarter - blog at https://living-smarter.com */}
         <article className="bg-wht20 sm:w-full md:w-auto p-4 text-center border-solid border-2 border-medDarkBlue rounded">
-          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-white">
-            Welcome to Luna Repair System!
-          </h1>
+          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-white">Welcome to Luna Repair System!</h1>
         </article>
         <article className="bg-eggShell p-5 sm:w-3/4 md:w-2/3 lg:w-2/5 flex flex-col items-center rounded">
           <div className="landing-page-row1 flex flex-col p-3 w-full text-center">
@@ -38,9 +37,7 @@ export default function Home() {
               className="shadow p-1 rounded"
               placeholder="We won't tell..."
             /> */}
-            <h3 className="text-xl">
-              Use your favorite social account or create an account using your email!
-            </h3>
+            <h3 className="text-xl">Use your favorite social account or create an account using your email!</h3>
             {!session && (
               <a
                 href="/auth/signin"
@@ -48,14 +45,14 @@ export default function Home() {
                 rel="noopener noreferrer"
                 onClick={(e) => {
                   e.preventDefault();
-                  {/* This needs to be updated so that users are redirected no matter what login flow they choose */}
+                  {
+                    /* This needs to be updated so that users are redirected no matter what login flow they choose */
+                  }
                   signIn();
-                }}
-              >
+                }}>
                 <button
                   type="button"
-                  className="bg-lightBlue text-white w-2/5 self-center hover:bg-darkBlue hover:shadow-md active:bg-lime active:text-charcoal focus:outline-none focus:shadow-outline font-bold mt-10 p-2 rounded shadow"
-                >
+                  className="bg-lightBlue text-white w-2/5 self-center hover:bg-darkBlue hover:shadow-md active:bg-lime active:text-charcoal focus:outline-none focus:shadow-outline font-bold mt-10 p-2 rounded shadow">
                   Sign In
                 </button>
               </a>
@@ -68,12 +65,10 @@ export default function Home() {
                 onClick={(e) => {
                   e.preventDefault();
                   signOut();
-                }}
-              >
+                }}>
                 <button
                   type="button"
-                  className="bg-black text-white w-2/5 self-center hover:bg-darkBlue hover:shadow-md active:bg-lime active:text-charcoal focus:outline-none focus:shadow-outline font-bold mt-10 p-2 rounded shadow"
-                >
+                  className="bg-black text-white w-2/5 self-center hover:bg-darkBlue hover:shadow-md active:bg-lime active:text-charcoal focus:outline-none focus:shadow-outline font-bold mt-10 p-2 rounded shadow">
                   Sign Out
                 </button>
               </a>
