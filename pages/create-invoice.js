61f9897e4b81036957d6924ca2c20a7679321146
Jonathan Sexton <joncsexton@gmail.com>
2020-10-25 15:37:08 -0500

    added more placeholder content & props

--- pages/create-invoice.js
+++ pages/create-invoice.js
@@ -1,34 +1,119 @@
-import React from 'react';
-import DashboardLayout from '../components/Layout/dashboardLayout';
+import React from "react";
+import DashboardLayout from "../components/Layout/dashboardLayout";
 
-const Input = (props) => (
-  <input {...props} className="border border-gray-300 p-1" />
-);
+const Input = (props) => <input {...props} className="border border-gray-300 p-1" />;
 
-const H2 = ({ children }) => (
-  <h2 className="text-2xl leading-7 font-semibold u">{children}</h2>
-);
+const H2 = ({ children }) => <h2 className="text-2xl leading-7 font-semibold u">{children}</h2>;
 
 const invoicePlaceholders = [
-  { invoiceNumber: '001', amount: '$500.00' },
-  { invoiceNumber: '002', amount: '$300.00' },
-  { invoiceNumber: '003', amount: '$400.00' },
-  { invoiceNumber: '004', amount: '$300.00' },
-  { invoiceNumber: '005', amount: '$1000.00' },
+  {
+    invoiceNumber: "001",
+    customerName: "James Madison",
+    customerCompany: "Company Name",
+    amount: "$500.35",
+    status: "Awaiting Payment",
+    created: "08/18/2020",
+    lastUpdated: "10/20/2020",
+  },
+  {
+    invoiceNumber: "002",
+    customerName: "Oprah Winnefry",
+    customerCompany: "Name of Company",
+    amount: "$300.28",
+    status: "Active",
+    created: "09/01/2020",
+    lastUpdated: "10/15/2020",
+  },
+  {
+    invoiceNumber: "003",
+    customerName: "John Smith",
+    customerCompany: "Company Name Here",
+    amount: "$400.71",
+    status: "Billed",
+    created: "6/15/2020",
+    lastUpdated: "10/25/2020",
+  },
+  {
+    invoiceNumber: "004",
+    customerName: "Jane Smith",
+    customerCompany: "Smith & Co.",
+    amount: "$300.85",
+    status: "Billed",
+    created: "06/27/2020",
+    lastUpdated: "10/03/2020",
+  },
+  {
+    invoiceNumber: "005",
+    customerName: "Calvin Klein",
+    customerCompany: "Smell Goodz, Inc.",
+    amount: "$1,000.94",
+    status: "Awaiting Payment",
+    created: "8/19/2020",
+    lastUpdated: "10/18/2020",
+  },
+  {
+    invoiceNumber: "006",
+    customerName: "Margaret Thatcher",
+    customerCompany: "UK Government, Inc.",
+    amount: "$1,200.07",
+    status: "Awaiting Payment",
+    created: "05/19/2020",
+    lastUpdated: "9/19/2020",
+  },
+  {
+    invoiceNumber: "007",
+    customerName: "Janice Joplin",
+    customerCompany: "Whatever Company",
+    amount: "$225.50",
+    status: "Paid & Closed",
+    created: "3/19/2020",
+    lastUpdated: "09/18/2020",
+  },
+  {
+    invoiceNumber: "008",
+    customerName: "Joan of Ark",
+    customerCompany: "Put Name here",
+    amount: "$1,650.63",
+    status: "Awaiting Payment",
+    created: "8/19/2020",
+    lastUpdated: "05/13/2020",
+  },
+  {
+    invoiceNumber: "009",
+    customerName: "Jerry Garcia",
+    customerCompany: "Standard Company Name",
+    amount: "$961.48",
+    status: "Billed",
+    created: "1/19/2020",
+    lastUpdated: "10/18/2020",
+  },
+  {
+    invoiceNumber: "0010",
+    customerName: "Dolly Parton",
+    customerCompany: "Generic Company Name",
+    amount: "$100.61",
+    status: "Paid & Closed",
+    created: "5/19/2020",
+    lastUpdated: "11/18/2020",
+  },
 ];
 
 const CreateInvoice = () => {
   return (
     <div className="mx-10">
       <h1 className="text-4xl font-bold up">Invoices</h1>
+      <div>
+        <p>Active Invoices: {invoicePlaceholders.length}</p>
+        <p></p>
+      </div>
       {/* Invoice page Main content */}
       <main className="grid items-stretch gap-y-8 mt-5">
         <div className="border border-gray-400 p-4 shadow-md">
           <H2>Create An Invoice</H2>
           <form className="grid justify-start gap-y-2 mt-8">
             <label>
-              Invoice Number:&nbsp;
-              <Input type="text" placeholder="#####" id="invoiceNumber" />
+              Invoice#:&nbsp;
+              <Input type="text" placeholder="#####" id="invoiceNumber" />{" "}
             </label>
             <div className="flex gap-x-5">
               <label>
@@ -48,10 +133,7 @@ const CreateInvoice = () => {
               Address 2:&nbsp;
               <Input type="text" placeholder="Address" id="address1" />
             </label>
-            <button
-              type="submit"
-              className="border-2 border-black rounded-md p-1 uppercase tracking-wider font-bold"
-            >
+            <button type="submit" className="border-2 border-black rounded-md p-1 uppercase tracking-wider font-bold">
               Submit
             </button>
           </form>
